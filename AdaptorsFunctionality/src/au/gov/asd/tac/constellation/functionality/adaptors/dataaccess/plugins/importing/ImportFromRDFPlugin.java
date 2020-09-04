a980f23f6b0335edc5f5a72673211100f7ed8db6
Nova-2119 <liam.banks@defence.gov.au>
2020-09-04 15:29:13 +0930

    :art: Generic and specific query versions. WORK IN PROGRESS

--- AdaptorsFunctionality/src/au/gov/asd/tac/constellation/functionality/adaptors/dataaccess/plugins/importing/ImportFromRDFPlugin.java
+++ AdaptorsFunctionality/src/au/gov/asd/tac/constellation/functionality/adaptors/dataaccess/plugins/importing/ImportFromRDFPlugin.java
@@ -16,7 +16,9 @@
 package au.gov.asd.tac.constellation.functionality.adaptors.dataaccess.plugins.importing;
 
 import au.gov.asd.tac.constellation.graph.processing.GraphRecordStore;
+import au.gov.asd.tac.constellation.graph.processing.GraphRecordStoreUtilities;
 import au.gov.asd.tac.constellation.graph.processing.RecordStore;
+import au.gov.asd.tac.constellation.graph.schema.visual.concept.VisualConcept;
 import au.gov.asd.tac.constellation.plugins.Plugin;
 import au.gov.asd.tac.constellation.plugins.PluginException;
 import au.gov.asd.tac.constellation.plugins.PluginInfo;
@@ -29,22 +31,23 @@ import au.gov.asd.tac.constellation.views.dataaccess.templates.RecordStoreQueryP
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.logging.Logger;
 import org.eclipse.rdf4j.RDF4JException;
-import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
-import org.eclipse.rdf4j.repository.sail.SailRepository;
-import org.eclipse.rdf4j.sail.memory.MemoryStore;
 
 /**
  *
@@ -56,6 +59,8 @@ import org.eclipse.rdf4j.sail.memory.MemoryStore;
 @PluginInfo(pluginType = PluginType.IMPORT, tags = {"IMPORT"})
 @NbBundle.Messages("ImportFromRDFPlugin=Import From RDF")
 public class ImportFromRDFPlugin extends RecordStoreQueryPlugin implements DataAccessPlugin {
+
+    private static final Logger LOG = Logger.getLogger(ImportFromRDFPlugin.class.getName());
 
     @Override
     protected RecordStore query(RecordStore query, PluginInteraction interaction, PluginParameters parameters) throws InterruptedException, PluginException {
@@ -82,23 +87,49 @@ public class ImportFromRDFPlugin extends RecordStoreQueryPlugin implements DataA
         } catch (RDF4JException e) {
             // handle exception
         }
-
+        
+        
+        HashMap<String, String> prefixes = new HashMap<>();
+        prefixes.put("country", "http://eulersharp.sourceforge.net/2003/03swap/countries#");
+        prefixes.put("foaf", "http://xmlns.com/foaf/0.1/");
+        prefixes.put("jur", "http://sweet.jpl.nasa.gov/2.3/humanJurisdiction.owl#");
+        prefixes.put("dce", "http://purl.org/dc/elements/1.1/");
+        prefixes.put("dct", "http://purl.org/dc/terms/");
+        prefixes.put("owl", "http://www.w3.org/2002/07/owl#");
+        prefixes.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+        prefixes.put("skos", "http://www.w3.org/2004/02/skos/core#");
+        
+        GraphRecordStore results = new GraphRecordStore();
         try (RepositoryConnection conn = repo.getConnection()) {
-            String queryString = "SELECT ?x ?y WHERE { ?x ?p ?y } ";
+            StringBuilder queryPrefix = new StringBuilder();
+//            TODO: Write something to automatically create the PREFIX part of the query based on the prefixes HashMap
+            
+//            String queryString = "PREFIX country: <" + COUNTRY + "> PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?Subject ?Country WHERE { ?Subject foaf:name ?Country} "; // For specific Country query/results
+            String queryString = "PREFIX country: <" + COUNTRY + "> PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?Subject ?Predicate ?Object WHERE { ?Subject ?Predicate ?Object} "; // Generic query
             TupleQuery tupleQuery = conn.prepareTupleQuery(queryString);
             try (TupleQueryResult result = tupleQuery.evaluate()) {
                 while (result.hasNext()) {  // iterate over the result
                     BindingSet bindingSet = result.next();
-                    Value valueOfX = bindingSet.getValue("x");
-                    Value valueOfY = bindingSet.getValue("y");
+//                    String bigraph = bindingSet.getValue("Subject").stringValue().split(COUNTRY)[1].toString(); //Specific
+////                    Value valueOfPredicate = bindingSet.getValue("Predicate");//Specific
+//                    String country = bindingSet.getValue("Country").stringValue();//Specific
 
-                    System.out.println(valueOfX.stringValue());
-                    System.out.println(valueOfY.stringValue());
+//                    results.add();//Specific
+//                    results.set(GraphRecordStoreUtilities.SOURCE + VisualConcept.VertexAttribute.IDENTIFIER, bigraph);//Specific
+//                    results.set(GraphRecordStoreUtilities.SOURCE + AnalyticConcept.VertexAttribute.TYPE, AnalyticConcept.VertexType.COUNTRY);//Specific
+//                    results.set(GraphRecordStoreUtilities.SOURCE + SpatialConcept.VertexAttribute.COUNTRY, country);//Specific
+                    String subject = bindingSet.getValue("Subject").toString(); //Generic
+                    String predicate = bindingSet.getValue("Predicate").toString();//Generic
+                    String object = bindingSet.getValue("Object").stringValue();//Generic
+                    //TODO: Automatically remove prefixes from source, predicate, object
+                    results.add();//Generic
+                    results.set(GraphRecordStoreUtilities.SOURCE + VisualConcept.VertexAttribute.IDENTIFIER, subject);//Generic
+                    results.set(GraphRecordStoreUtilities.SOURCE + predicate, object);//Generic
                 }
             }
         }
 
-        return new GraphRecordStore();
+        return results;
     }
 
     @Override
