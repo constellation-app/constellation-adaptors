58a587b193f8f07c01e3e4ee9a4564a38522120c
GCHQDeveloper601 <55090814+GCHQDeveloper601@users.noreply.github.com>
2020-09-25 06:38:30 +0100

    Gaffer module (#9)
    * Adding Gaffer Module
    
    * Removing defunct folder
    
    * Adding readme
    
    * Add Gaffer Types
    
    * Adding config to add dependancies
    
    * Refactored to use a dropdown for choosing the query to perform
    
    * improved the query to a simpler form
    
    * Corrected version that will run from Netbeans directly
    
    * Upgrading to Netbeans 12, also solves some classpath issues by removing Module dependancy on CoreDependancy
    
    * Added unit test dependancies and a unit test for Gaffer. Would like to add Mock test but would need more dependancies for this
    
    * Added mockito and mock tests against the gaffer rest service without it running
    
    * Made changes as per the code review, also a line to whatsnew.txt
    
    * Readded the Data Access Plugin module after a hasty refactor
    
    * Refactored a class name for consistency
    
    * Adding the refactored file
    
    * Adding missing bundle and project files
    
    * Updated based on review
    * Renamed the Adaptors Data Access View package from constellations to constelletion
    * Removed and excluded some more dependencies. Testing via the unit test seems to work OK.
    
    Co-authored-by: GCHQDeveloper01001010 <57718619+GCHQDeveloper01001010@users.noreply.github.com>
    Co-authored-by: arcturus2 <39325530+arcturus2@users.noreply.github.com>

--- AdaptorsDataAccessPlugins/src/au/gov/asd/tac/constellation/dataaccess/adaptors/DataAccessPluginAdaptorType.java
+++ AdaptorsDataAccessPlugins/src/au/gov/asd/tac/constellation/dataaccess/adaptors/DataAccessPluginAdaptorType.java
@@ -1,21 +1,20 @@
 /*
- * Copyright 2010-2019 Australian Signals Directorate
- *
+ * Copyright 2010-2020 Australian Signals Directorate
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package au.gov.asd.tac.constellation.functionality.adaptors.dataaccess.plugins;
+package au.gov.asd.tac.constellation.dataaccess.adaptors;
 
-import au.gov.asd.tac.constellation.views.dataaccess.DataAccessPluginCoreType;
 import au.gov.asd.tac.constellation.views.dataaccess.DataAccessPluginType;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,19 +22,19 @@ import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
- * @author canis_majoris
+ * @author GCHQDeveloper601
  */
 @ServiceProvider(service = DataAccessPluginType.class)
-public class DataAccessPluginAdaptorType extends DataAccessPluginCoreType {
+public class DataAccessPluginAdaptorType extends DataAccessPluginType {
 
-    public static final String HOP = "Hop";
-    public static final String ENRICH = "Enrich";
+    public static final String EXAMPLE = "Example";
 
     @Override
     public List<PositionalDataAccessPluginType> getPluginTypeList() {
         final ArrayList<PositionalDataAccessPluginType> pluginTypeList = new ArrayList<>();
-        pluginTypeList.add(new DataAccessPluginType.PositionalDataAccessPluginType(ENRICH, 1400));
-        pluginTypeList.add(new DataAccessPluginType.PositionalDataAccessPluginType(HOP, 1500));
+
+        pluginTypeList.add(new DataAccessPluginType.PositionalDataAccessPluginType(EXAMPLE, 2500));
         return pluginTypeList;
     }
+
 }
