6707c8c8488057cbbdbdf5324e525787a83dfea7
antares1470 <62311243+antares1470@users.noreply.github.com>
2021-08-19 12:40:20 +1000

    updated data access help pages, moved to appropriate module

--- AdaptorsDataAccessPlugins/src/au/gov/asd/tac/constellation/views/dataaccess/adaptors/docs/package-info.java
+++ AdaptorsDataAccessPlugins/src/au/gov/asd/tac/constellation/views/dataaccess/adaptors/docs/package-info.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010-2019 Australian Signals Directorate
- *
+ * Copyright 2010-2021 Australian Signals Directorate
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 @HelpSetRegistration(helpSet = "adaptors-hs.xml", position = 0)
-package au.gov.asd.tac.constellation.functionality.adaptors.docs;
+package au.gov.asd.tac.constellation.views.dataaccess.adaptors.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,15 @@
 # Constellation Adaptors Changes
 
-## 2020-02-01 Changes in February 2020
+## Changes in August 2021
+* Moved help pages for Data Access Plugins from `au.gov.asd.tac.constellation.functionality.adaptors` 
+to `au.gov.asd.tac.constellation.views.dataaccess.adaptors.docs`
+
+## Changes in February 2020
 * Added the Constellation branding to the Adaptors module
 * Fixed to the `build.xml` script to generate a build zip
 * Fixed to the module suite dependency names to work with the default checkout
 
-## 2019-12-01 Changes in December 2019
+## Changes in December 2019
 * Added the `AdaptorsDependencies` module
 * Added the `AdaptorsMapView` module
 * Added the new Adaptors Functionality module
